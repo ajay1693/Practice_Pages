9a1159509d94bc4dc1e2f9ff3f45779c0a95c65a
Ajay Adsule <96196563+ajay1693@users.noreply.github.com>
2022-06-29 15:51:11 +0530

    popup

---
+++ Popup_login/popup.js
@@ -0,0 +1,9 @@
+function changeLog(){
+    document.getElementById("PopupLeft1").style.display = "block";
+    document.getElementById("PopupLeft2").style.display = "none";
+}
+
+function changeLog2(){
+    document.getElementById("PopupLeft1").style.display = "none";
+    document.getElementById("PopupLeft2").style.display = "block";
+}
